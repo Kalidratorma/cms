e8d2ee5098048ef8267259558c0481f0b8cb0069
kalidratorma <kalidratorma@gmail.com>
2023-03-09 21:47:56 +0300

    added ContentHashMapConverter, added lombok

--- src/main/java/com/kalidratorma/cms/core/site/Section.java
+++ src/main/java/com/kalidratorma/cms/core/site/Section.java
@@ -1,11 +1,22 @@
 package com.kalidratorma.cms.core.site;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnTransformer;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+import java.util.Map;
+
 @Entity
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
 @Table(name = "section")
 public class Section {
     @TableGenerator(
@@ -23,79 +34,12 @@ public class Section {
     private String title;
     private String description;
 
-    @Column(columnDefinition="jsonb")
-    private Object content;
+    @Column(columnDefinition = "jsonb")
+    @Convert(converter = ContentHashMapConverter.class)
+    @ColumnTransformer(write = "?::jsonb")
+    private Map<String, Object> content;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JsonBackReference
     private Page page;
-
-    public Section() {
-    }
-
-    public Section(String name, String title, String description, Object content) {
-        this.name = name;
-        this.title = title;
-        this.description = description;
-        this.content = content;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Object getContent() {
-        return content;
-    }
-
-    public void setContent(Object content) {
-        this.content = content;
-    }
-
-    public Page getPage() {
-        return page;
-    }
-
-    public void setPage(Page page) {
-        this.page = page;
-    }
-
-    @Override
-    public String toString() {
-        return "Section{" +
-                "name='" + name + '\'' +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", content='" + content + '\'' +
-                ", page=" + page +
-                '}';
-    }
 }
