1ee2eac298e76879b9f568fc50bdcf0d3de149ca
kalidratorma <kalidratorma@gmail.com>
2023-03-11 20:36:57 +0300

    added security with jwt

---
+++ src/main/java/com/kalidratorma/cms/core/security/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.kalidratorma.cms.core.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/*")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "/site/*")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "/site").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.DELETE).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/site/*").hasAnyAuthority("ADMIN","MODERATOR")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
---
+++ src/main/java/com/kalidratorma/cms/core/security/user/Role.java
@@ -0,0 +1,8 @@
+package com.kalidratorma.cms.core.security.user;
+
+public enum Role {
+    USER,
+    ADMIN,
+    SITE,
+    MODERATOR
+}
