cc97ed4a3cd031e46842d27e9d9b2323a4de531d
kalidratorma <kalidratorma@gmail.com>
2023-03-14 22:30:15 +0300

    fixed CORS error

--- src/main/java/com/kalidratorma/cms/core/security/SecurityConfiguration.java
+++ src/main/java/com/kalidratorma/cms/core/security/SecurityConfiguration.java
@@ -31,6 +31,7 @@ public class SecurityConfiguration {
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
